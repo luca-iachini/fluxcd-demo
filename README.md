702de3e5d2bc86d690da5248056127e216f710ef
luca-iachini <luca.iachini@patchai.io>
2021-10-25 15:13:25 +0200

    feat: add github ssh secret

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # fluxcd-test
-fluxcd test
+
+fluxcd test repo
+
+## Install
+
+Execute all the scripts in te fluxcd folder:
+
+- `1-docker-registry.sh` add a local registry
+- `2-install-flux.sh` add flux controllers to kubernetes
+- `3-dev-env-init.sh` configure the dev env to deploy a test app.
+
+## Useful commands
+
+Get fluxcd git sources
+
+```sh
+watch flux get sources git
+```
--- fluxcd/3-dev-env-init.sh
+++ fluxcd/3-dev-env-init.sh
@@ -1,4 +1,9 @@
-flux create source git dev --url ssh://git@github.com/luca-iachini/fluxcd-test.git --username $GITHUB_USER --password $GITHUB_TOKEN \
+kubectl create secret -n flux-system generic github-ssh-credentials \
+    --from-literal identity="$(cat $GITHUB_IDENTITY)" \
+    --from-literal identity.pub="$(cat $GITHUB_IDENTITY.pub)" \
+    --from-literal known_hosts="$(ssh-keyscan github.com)"
+
+flux create source git dev --url ssh://git@github.com/luca-iachini/fluxcd-test.git --secret-ref github-ssh-credentials \
  --branch main --interval 30s --export | tee apps/dev.yaml
 
  flux create kustomization dev --source dev --path "./fluxcd/dev/apps/" --prune true \
