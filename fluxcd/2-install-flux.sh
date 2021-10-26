07175bb632ba922587095934a0389d1935cf6c2a
luca-iachini <luca.iachini@patchai.io>
2021-10-26 12:06:44 +0200

    feat: add image updater

--- fluxcd/2-install-flux.sh
+++ fluxcd/2-install-flux.sh
@@ -6,3 +6,14 @@ flux bootstrap github \
   --path=fluxcd/apps \
   --read-write-key \
   --personal
+
+
+flux create image update flux-system \
+--git-repo-ref=flux-system \
+--git-repo-path="./fluxcd/apps" \
+--checkout-branch=main \
+--push-branch=main \
+--author-name=fluxcdbot \
+--author-email=fluxcdbot@users.noreply.github.com \
+--commit-template="{{range .Updated.Images}}{{println .}}{{end}}" \
+--export > ./apps/flux-system-automation.yaml
