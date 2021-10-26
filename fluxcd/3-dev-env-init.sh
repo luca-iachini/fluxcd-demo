ffc0bb7fa8c3769b53cc18e238d5940c5b33f1bc
luca-iachini <luca.iachini@patchai.io>
2021-10-26 13:15:28 +0200

    refactor: add comments to dev init script

--- fluxcd/3-dev-env-init.sh
+++ fluxcd/3-dev-env-init.sh
@@ -1,21 +1,28 @@
+#create Github secrets
 kubectl create secret -n flux-system generic github-ssh-credentials \
     --from-literal identity="$(cat $GITHUB_IDENTITY)" \
     --from-literal identity.pub="$(cat $GITHUB_IDENTITY.pub)" \
     --from-literal known_hosts="$(ssh-keyscan github.com)"
 
+#add flux git souce for dev env configurations
 flux create source git dev --url ssh://git@github.com/luca-iachini/fluxcd-test.git --secret-ref github-ssh-credentials \
  --branch main --interval 30s --export | tee apps/dev.yaml
 
- flux create kustomization dev --source dev --path "./fluxcd/dev/apps/" --prune true \
+#create flux configurations for dev env
+flux create kustomization dev --source dev --path "./fluxcd/dev/apps/" --prune true \
   --interval 10m --export | tee -a apps/dev.yaml
 
-flux create helmrelease app1 --source GitRepository/dev --values ../helm/app1/values.dev.yaml --chart "./charts/go" --target-namespace dev \
-  --interval 30s --export | tee dev/apps/app1.yaml
+#add helm release for the demo app
+flux create helmrelease app1 --source GitRepository/dev --values ../helm/app1/values.dev.yaml --chart "./charts/go" \
+  --create-target-namespace --target-namespace dev \
+  --interval 30s --export | sed -E 's/(tag: .*)/\1 # {"$imagepolicy": "flux-system:dev-app1:tag"}/g' | tee dev/apps/app1.yaml
 
+#create image repository for the demo app
 flux create image repository app1 --image=$DOCKER_REG_URL/fluxcd-test/app1 \
 --interval=1m --secret-ref regcred \
 --export > ./dev/apps/app1-registry.yaml
 
+#create image policy for the demo app. Con
 #flux create image policy dev-app1 \
 #--image-ref=app1 \
 #--select-semver=0.0.x \
@@ -28,7 +35,7 @@ flux create image policy dev-app1 \
 --filter-extract='$ts' \
 --export > ./dev/apps/app1-policy.yaml
 
-
+# configure flux write back to update the helm release manifest
 flux create image update dev-apps \
 --git-repo-ref=dev \
 --git-repo-path="./fluxcd/dev/apps" \
