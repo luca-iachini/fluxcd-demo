cfb98ff46b8052a13411357721d1298bcf27b53c
luca-iachini <luca.iachini@patchai.io>
2021-10-27 11:32:50 +0200

    refactor: add docker registry patch

--- fluxcd/2-install-flux.sh
+++ fluxcd/2-install-flux.sh
@@ -6,3 +6,11 @@ flux bootstrap github \
   --path=fluxcd/apps \
   --read-write-key \
   --personal
+
+git pull
+
+## add docker registry certs
+$(cd apps/flux-system && \
+  kustomize edit add patch --path docker-certs.yaml \
+  --kind Deployment --name image-reflector-controller \
+  --namespace flux-system --version apps/v1)
